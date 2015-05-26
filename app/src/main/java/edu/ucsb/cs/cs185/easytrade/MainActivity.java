d2fed08a2e131cfb558d8969cf9a87aa4d4a5a74
wangyixin1992 <yixinwang@umail.ucsb.edu>
2015-05-27 07:58:08 +0800

    test 2

--- app/src/main/java/edu/ucsb/cs/cs185/easytrade/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/easytrade/MainActivity.java
@@ -1,20 +1,22 @@
 package edu.ucsb.cs.cs185.easytrade;
 
-import android.os.Bundle;
+import java.util.Locale;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.Locale;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
@@ -33,9 +35,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      * The {@link ViewPager} that will host the section contents.
      */
     ViewPager mViewPager;
-
-
-    /////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
