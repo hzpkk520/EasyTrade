46517745a133c902d020066b684bd1c8ede8d88d
hzpkk520 <hzpkk520@gmail.com>
2015-05-26 16:33:04 -0700

    Test commit

--- app/src/main/java/edu/ucsb/cs/cs185/easytrade/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/easytrade/MainActivity.java
@@ -36,6 +36,9 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      */
     ViewPager mViewPager;
 
+
+    ////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
